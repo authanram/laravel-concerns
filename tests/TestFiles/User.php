645329c96404c18f83dd43250ff9aa6e6b365400
Daniel Seuffer <authanram@gmail.com>
2022-02-16 16:36:55 +0100

    WIP

---
+++ tests/TestFiles/User.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Authanram\LaravelConcerns\Tests;
+
+use FoobarTools\Console\Concerns\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+    use HasUuid;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
